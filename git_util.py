401ce055137dbce5b1a022b75d2345ae7f7c5e9f
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2017-03-13 20:54:12 +0900

    added git_rebase_verbose() : git rebase --verbose remote/branch

--- git_util.py
+++ git_util.py
@@ -239,6 +239,10 @@ def git_fetch(remote_name):
     return git('fetch %s' % remote_name)
 
 
+def git_rebase_verbose(remote, branch):
+    return git('rebase --verbose %s/%s' % (remote, branch))
+
+
 def recursively_process_path(path):
     for root, dirs, files in os.walk(path):
         if ".git" in dirs:
