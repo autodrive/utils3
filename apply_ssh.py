36b943669c68ea2e5d0b4ce4a07ae4c29e515549
KangWon Lee <kangwon.lee@kpu.ac.kr>
2017-04-14 23:40:51 +0900

    ApplySSH : apply ssh urls

--- apply_ssh.py
+++ apply_ssh.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import os
 import re
 import sys
 import urllib.parse
@@ -12,7 +13,17 @@ import git_util
 
 def main(argv):
     repo_path = argv[1]
-    updater = ApplySSH(repo_path, 'config')
+
+    if 3 > len(argv):
+        b_arm = False
+    elif 'false' in argv[2].lower():
+        b_arm = False
+    elif 'True' == argv[2]:
+        b_arm = True
+    else:
+        b_arm = False
+
+    updater = ApplySSH(repo_path, 'config', b_arm=b_arm)
     updater.recursively_find_in()
 
 
@@ -22,6 +33,7 @@ class ApplySSH(find_git_repos.RecursiveGitRepositoryFinderBase):
         self.finder = re.compile(r'https://.*' + repo_site_name + '/.+\.git')
         # if true, overwrite a new url
         self.b_arm = b_arm
+        self.start_path = os.path.abspath(os.curdir)
 
     def is_target(self, url):
         if url is not None:
@@ -38,15 +50,54 @@ class ApplySSH(find_git_repos.RecursiveGitRepositoryFinderBase):
             for remote_name, remote_info_dict in remote_info_dict_dict.items():
                 remote_url = remote_info_dict.get('url', None)
                 if self.is_target(remote_url):
+                    os.chdir(repo_path)
                     ssh_url = self.convert_url_https_to_ssh(remote_url)
-                    git_cmd = 'remote %s %s' % (remote_name, ssh_url)
-                    print(remote_name, remote_url, ssh_url, git_cmd)
+                    git_cmd, git_cmd_push = self.get_git_cmd_remote_set_url(remote_name, ssh_url, remote_url)
+                    if not self.b_arm:
+                        print(remote_name, remote_url, ssh_url)
+                        print(git_cmd)
+                        print(git_cmd_push)
+                    else:
+                        print("** updating %r **" % repo_path)
+                        print(remote_name, remote_url)
+                        git_util.git(git_cmd)
+                        git_util.git(git_cmd_push)
+                    os.chdir(self.start_path)
+
+    @staticmethod
+    def get_git_cmd_remote_set_url(remote_name, ssh_url, https_url):
+        """
+        usage: git remote [-v | --verbose]
+           or: git remote add [-t <branch>] [-m <master>] [-f] [--tags | --no-tags] [--mirror=<fetch|push>] <name> <url>
+           or: git remote rename <old> <new>
+           or: git remote remove <name>
+           or: git remote set-head <name> (-a | --auto | -d | --delete | <branch>)
+           or: git remote [-v | --verbose] show [-n] <name>
+           or: git remote prune [-n | --dry-run] <name>
+           or: git remote [-v | --verbose] update [-p | --prune] [(<group> | <remote>)...]
+           or: git remote set-branches [--add] <name> <branch>...
+           or: git remote get-url [--push] [--all] <name>
+           or: git remote set-url [--push] <name> <newurl> [<oldurl>]
+           or: git remote set-url --add <name> <newurl>
+           or: git remote set-url --delete <name> <url>
+
+            -v, --verbose         be verbose; must be placed before a subcommand
+
+        :param remote_name:
+        :param ssh_url:
+        :return:
+        """
+        git_cmd = 'remote -v set-url %s %s %s' % (remote_name, ssh_url, https_url)
+        git_cmd_push = 'remote -v set-url --push %s %s %s' % (remote_name, ssh_url, https_url)
+        return git_cmd, git_cmd_push
 
     @staticmethod
     def convert_url_https_to_ssh(https_url):
         """
         convert https url to ssh url mainly for bitbucket.org
         see : https://confluence.atlassian.com/bitbucket/set-up-ssh-for-git-728138079.html
+              https://<accountname>@bitbucket.org/< accountname>/<reponame>.git
+              ssh://git@bitbucket.org /< accountname>/<reponame>.git
 
         :param str https_url:
         :return:
