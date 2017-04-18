183348d0740e45add59cfb75707932495d5b88e6
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2017-04-18 14:21:30 +0900

    added comments

--- unique_hosts.py
+++ unique_hosts.py
@@ -15,17 +15,28 @@ def main(argv):
 
     # text line loop
     for i, host_line in enumerate(hosts_txt_lines):
+        # remove '\n' at the end
         host_line_strip = host_line.strip()
+        # if anything remaining
         if host_line_strip:
             if host_line_strip.startswith('#'):
+                # if comment
                 key = host_line_strip
+                # append line number
                 value = i
             else:
+                # if not comment
                 host_line_strip_split = host_line_strip.split()
+                # probably [address, domain name]
                 key = host_line_strip_split[-1]
+                # append line number and address
                 value = (i, host_line_strip_split[:-1])
+            # end if comment block
+            # find entry in d
             d[key] = d.get(key, [])
+            # append to the entry
             d[key].append(value)
+    # end text line loop
     print(len(d.keys()))
 
 
