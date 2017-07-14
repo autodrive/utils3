b76651d5330e8e5605e30cff387da9d4d4a65d70
KangWon Lee <kangwon_fwd@naver.com>
2017-07-14 09:43:22 +0900

    added git repository path interface

--- git_upstream_2_origin.py
+++ git_upstream_2_origin.py
@@ -1,10 +1,21 @@
+import os
+import sys
+
 import git_util
 
 
-def main():
-    remotes_list = git_util.git('remote')
+def main(argv):
+    # repo path
+    if 1 < len(argv):
+        repo_path = argv[1]
+    else:
+        repo_path = os.getcwd()
+
+    git_path_spec = "-C '%s'" % repo_path
+
+    remotes_list = git_util.git(' '.join((git_path_spec, 'remote')))
     print(remotes_list)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
