522263a8625ff21c795181e8bedb0e47c8f3e8eb
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-07-28 14:59:08 +0700

    removed one line of docstring

--- ipynb_remove_output.py
+++ ipynb_remove_output.py
@@ -1,7 +1,6 @@
 """
 Jupyter notebook could be good for educational purpose.
 Before release in class, output generated during testing and execution number need to be removed.
-Some people already wrote some scripts but have problem dealing with special characters  
 """
 
 import os
