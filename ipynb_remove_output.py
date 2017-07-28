c511e6f3b29cfa107309b19d360308dccc65282f
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-07-28 13:41:59 +0700

    nbformat : Use this module to read or write notebook files as particular nbformat versions.

--- ipynb_remove_output.py
+++ ipynb_remove_output.py
@@ -1,10 +1,14 @@
-import notebook
+"""
+Jupyter notebook could be good for educational purpose.
+Before release in class, output generated during testing and execution number need to be removed.
+Some people already wrote some scripts but have problem dealing with special characters  
+"""
+
 import nbformat
 
 
 if __name__ == '__main__':
     def main():
-        help(notebook)
         help(nbformat)
 
     main()
