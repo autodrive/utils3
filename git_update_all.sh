b61af3ca0f58c273e8269998dd6f1e204d48c837
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2018-06-19 13:05:58 +0900

    Designated path to git_update_all.sh

--- git_update_all.sh
+++ git_update_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # http://stackoverflow.com/questions/32597209/python-not-working-in-the-command-line-of-git-bash
 # http://how-to.wikia.com/wiki/How_to_read_command_line_arguments_in_a_bash_script
-winpty python git_update_all.py $1
+winpty python utils3/git_update_all.py $1
